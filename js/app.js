9546a273fcb6ba81e8fdcf83b8186717a0ef48cd
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-08 18:37:02 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,19 +1,13 @@
 const advice_id = document.getElementById('advice_id');
 const advice_slip = document.getElementById('advice_slip');
 
-function create_quotes() {
+async function create_quotes() {
     const http = new XMLHttpRequest();
     const quotes_id = Math.floor(Math.random() * 224) + 1;
-
-    http.open("GET", "	https://api.adviceslip.com/advice/" + quotes_id, true);
-    http.send();
-    http.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            const data = JSON.parse(this.responseText);
-            advice_id.innerHTML = data.slip.id;
-            advice_slip.innerHTML = data.slip.advice;
-        }
-    }
+    const response = await fetch(`https://api.adviceslip.com/advice/${quotes_id}`);
+    const data = await response.json();
+    advice_id.textContent = data.slip.id;
+    advice_slip.textContent = data.slip.advice;
 }
 
 
