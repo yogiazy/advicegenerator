4f5e7c0b7d94db2735c8c42e8172c96670b82a9b
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-08 18:37:52 +0700

    update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const advice_id = document.getElementById('advice_id');
 const advice_slip = document.getElementById('advice_slip');
 
 async function create_quotes() {
-    const http = new XMLHttpRequest();
     const quotes_id = Math.floor(Math.random() * 224) + 1;
     const response = await fetch(`https://api.adviceslip.com/advice/${quotes_id}`);
     const data = await response.json();
