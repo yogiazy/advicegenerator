b0957234ad665583fc30a92bfde00273b8785f5b
Yogi Prakoso <yogiarrazy@gmail.com>
2023-12-08 17:58:00 +0700

    update

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function create_quotes() {
+    const http = new XMLHttpRequest();
+    const quotes_id = Math.floor(Math.random() * 224) + 1;
+    const advice_id = document.getElementById('advice_id');
+    const advice_slip = document.getElementById('advice_slip');
+    http.open("GET", "	https://api.adviceslip.com/advice/" + quotes_id, true);
+    http.send();
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const data = JSON.parse(this.responseText);
+            advice_id.innerHTML = data.slip.id;
+            advice_slip.innerHTML = data.slip.advice;
+        }
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./index.html', './js/app.js'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        light_cyan: 'hsl(193, 38%, 86%)',
+        neon_green: 'hsl(150, 100%, 66%)',
+        grayish_blue: 'hsl(217, 19%, 38%)',
+        dark_grayish_blue: 'hsl(217, 19%, 24%)',
+        dark_blue: 'hsl(218, 23%, 16%)'
+      },
+      boxShadow: {
+        '3xl': '0 0 10px hsl(150, 100%, 66%)'
+      }
+    },
   },
   plugins: [],
 }
